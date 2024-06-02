760b9a2413c0cdc593e9716d30a6bccab417f09f
johhny1898 <134100252+johhny1898@users.noreply.github.com>
2024-06-03 00:00:14 +0330

    Update warp.py

--- warp.py
+++ warp.py
@@ -86,7 +86,7 @@ def warp_ip():
     return config_prefix, formatted_time
 
 
-title = "//profile-title: base64:" + base64.b64encode('Women Life Freedom ✌️'.encode('utf-8')).decode('utf-8') + "\n"
+title = "//profile-title: base64:" + base64.b64encode('Auto update ✌️'.encode('utf-8')).decode('utf-8') + "\n"
 update_interval = "//profile-update-interval: 1\n"
 sub_info = "//subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=2546249531\n"
 profile_web = "//profile-web-page-url: https://github.com/ByteMysticRogue\n"
